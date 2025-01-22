a2c28083014b4edd749823fd7b41c6b3f7ccf940
clarisse melquiades <clarisse.melquiades@academico.ifpb.edu.br>
2025-01-22 10:44:45 -0300

    biblioteca

---
+++ dao/__init__.py
@@ -0,0 +1,61 @@
+import psycopg2
+
+def conectardb():
+
+    con = psycopg2.connect(
+
+        host='localhost',
+        database = 'meninas',
+        user = 'postgres',
+        password = '12345'
+    )
+    return con
+
+
+def login(user,senha):
+    con = conectardb()
+    cur = con.cursor()
+    sq = f"SELECT nome, login from usuario where login='{user}' and senha='{senha}'  "
+    cur.execute(sq)
+    saida = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return saida
+
+def inserir_user(nome, login, senha):
+
+    conn = conectardb()
+    cur = conn.cursor()
+    try:
+        sql = f"INSERT INTO usuario (nome, senha, login) VALUES ('{nome}','{senha}', '{login}' )"
+        cur.execute(sql)
+
+
+
+    except psycopg2.IntegrityError:
+        conn.rollback()
+        exito = False
+    else:
+        conn.commit()
+        exito = True
+
+    cur.close()
+    conn.close()
+    return exito
+
+
+def listar_livros(login):
+    con = conectardb()
+    cur = con.cursor()
+    sq = f"SELECT * from resenha WHERE login='{login}'"
+    cur.execute(sq)
+    saida = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return saida
+
+print(listar_livros('mari'))
---
+++ servidor.py
@@ -0,0 +1,65 @@
+from flask import*
+
+import dao
+
+app = Flask(__name__)
+app.secret_key = 'ajhk23h423'
+
+
+@app.route("/")
+def paginicial():
+    return render_template('inicio2.html')
+
+@app.route('/enviar-dados' , methods =['POST', 'GET'])
+def paglivros():
+
+    if request.method == 'POST':
+
+        login = request.form.get('nome')
+        senha = request.form.get('senha')
+
+
+        if len(dao.login(login,senha)) > 0:
+            session['login'] = login
+            return render_template('livros.html')
+        else:
+            return render_template('inicio2.html', msg='usuário ou senha incorretos')
+
+    else:
+        if 'login' in session:
+            return render_template('livros.html')
+        else:
+            return render_template('inicio2.html')
+
+@app.route("/pagcadastro")
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route("/cadastrar_user",methods=['POST'])
+def fazercadastro():
+    nome = request.form.get('nome completo')
+    usuario = request.form.get('usuario')
+    senha = request.form.get('senha')
+
+    if dao.inserir_user(nome, usuario, senha):
+     msg = 'usuario inserido com sucesso'
+     return render_template('inicio2.html' , texto=msg)
+
+    else:
+        msg = 'usuario nao inserido'
+        return render_template('inicio2.html', texto=msg)
+
+
+@app.route("/listarlivros")
+def lista():
+    login = session['login']
+    resenhas = dao.listar_livros(login)
+
+    return render_template('lista.html', lista=resenhas)
+
+
+
+
+if __name__ == '__main__':
+        app.run(debug=True)
+
